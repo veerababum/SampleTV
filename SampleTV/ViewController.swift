e3be1a1a8361c59810b147dea72856a37da91975
VMulugu1 <veerababu.mulugu1@T-Mobile.com>
2020-09-16 11:59:42 -0700

    Integrated tableview

--- SampleTV/ViewController.swift
+++ SampleTV/ViewController.swift
@@ -8,13 +8,79 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+       
+    let namesArray = ["Veera","babu","Krishna","kumari","koti","Abhi"]
+    let namesDetailsArray = ["23453","32453","234523","2345","koti","Abhi"]
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    // MARK: - View Controller methods
+
+       override func viewDidLoad() {
+           super.viewDidLoad()
+           // Do any additional setup after loading the view.
+           tableView.delegate = self
+           tableView.dataSource = self
+//           tableView.backgroundColor = UIColor.red
+        
+        for i in 0...namesArray.count - 1 {
+            
+            print(namesArray[i])
+        }
+        
+           
+       }
+
+
+    
+    // MARK: - UITableview Datasource and delegate methods
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return namesArray.count
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellIdentifer")
+        cell?.textLabel?.text = "The SNO \(indexPath.row): Name \(namesArray[indexPath.row])"
+        cell?.detailTextLabel?.text = namesDetailsArray[indexPath.row]
+//        cell?.backgroundColor = .gray
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Header Section Title"
+    }
+    func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+           return "TitleForFooterInSection Title"
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 75.00
+    }
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        
+        (view as! UITableViewHeaderFooterView).contentView.backgroundColor = UIColor.black.withAlphaComponent(0.4)
+        
+        (view as! UITableViewHeaderFooterView).textLabel?.textColor = UIColor.white
+    }
+    func tableView(_ tableView: UITableView, willDisplayFooterView view: UIView, forSection section: Int) {
+           (view as! UITableViewHeaderFooterView).contentView.backgroundColor = UIColor.black.withAlphaComponent(0.4)
+             
+             (view as! UITableViewHeaderFooterView).textLabel?.textColor = UIColor.white
+    }
+    
+//    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+//        (view as! UITableViewHeaderFooterView).contentView.backgroundColor = UIColor.black.withAlphaComponent(0.4)
+//        (view as! UITableViewHeaderFooterView).textLabel?.textColor = UIColor.white
+//
+//    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50.0
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+   
 }
 
